7458878230dcdcf8dba7bbea5f44b94adcb05cfc
Abhiroop Sarkar <73867798+Abhiroop2004@users.noreply.github.com>
2024-02-04 16:44:18 +0530

    Generate Random numbers to a txt file

---
+++ rng.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n;
+    FILE *r=fopen("random.txt","w");
+    printf("Enter length of array:  ");
+    scanf("%d",&n);
+    int i,a[n];
+    for (i=0; i<n; i++)
+    {
+        a[i]=rand()%10000;
+        fprintf(r,"%d\n",a[i]);
+    }
+    fclose(r);
+    printf("Completed!");
+}
